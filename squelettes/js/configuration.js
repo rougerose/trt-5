7642838c3997717b544af10cbd8b32106c70a3f6
christophe le drean <christophe@rouge-rose.net>
2011-01-21 11:34:18 +0100

    debut

---
+++ squelettes/js/configuration.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    var options = {
+        assetURL:"squelettes/js/shadowbox/",
+        keysClose:['c', 27] // c or esc
+        };
+Shadowbox.init(options);
+
+});
