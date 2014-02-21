00d0b5f0162d79ec903a0c6f27b71c2f5c213ddf
christophe le drean <christophe@rouge-rose.net>
2014-02-21 20:34:36 +0100

    base pour la version dev

--- squelettes/js/configuration.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-    var options = {
-        assetURL:"squelettes/js/shadowbox/",
-        keysClose:['c', 27] // c or esc
-        };
-Shadowbox.init(options);
-
-});
