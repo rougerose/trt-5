fc98a90940e470400cd5fab3ff9cdd08b92f5ebd
christophe le drean <christophe@rouge-rose.net>
2014-04-03 09:21:30 +0200

    rubrique

--- lang/local_fr.php
+++ lang/local_fr.php
@@ -1,6 +1,13 @@
 <?php
 if (!defined('_ECRIRE_INC_VERSION')) return;
 $GLOBALS[$GLOBALS['idx_lang']] = array(
+    // A
+    'actualites' => "Actualités",
+    'articles_tous' => "Tous les articles",
+
+    // C
+    'consultez_rubrique' => "Consultez la rubrique",
+
     // D
     'documents_image' => "Images associées",
     'documents_fichier' => "Fichiers associés",
