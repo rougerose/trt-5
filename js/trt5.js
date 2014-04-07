1ac626d014aabe23a6f3ba06447dc3694e46203f
christophe le drean <christophe@rouge-rose.net>
2014-04-07 18:02:46 +0200

    accueil : présentation trt-5

--- js/trt5.js
+++ js/trt5.js
@@ -6,16 +6,14 @@ $(function() {
     // Slider Foundation 
     // ======================================
     // http://foundation.zurb.com/forum/posts/417-orbit-jumps-around-on-load
-    $("#orbit").hide(0).fadeIn(2000);
+    //$("#orbit-selection","#orbit-trt5").hide(0).fadeIn(10000);
 
     $(document).foundation({
         orbit: {
             timer_speed: 10000,
             slide_number: false,
             slide_selector: 'li',
-            pause_on_hover: false, // Pauses on the current slide while hovering
-            //resume_on_mouseout: false, // If pause on hover is set to true, this setting resumes playback after mousing out of slide
-            //next_on_click: true, // Advance to next slide on click
+            pause_on_hover: false,
             animation_speed: 500
         }
     });
