d7fd71ac68d19ca7637e8675d3b4ea67db269018
christophe le drean <christophe@rouge-rose.net>
2014-08-14 18:22:18 +0200

    corrections

--- js/trt5.js
+++ js/trt5.js
@@ -20,54 +20,11 @@ $(function() {
             nav.removeClass("active").eq(id).addClass("active");
         });
     }
-    
-    
-    
-
-    
-    
-    // if ($("#carouselPresentation").length) {
-    //     var $nav = $("#carouselNav > a");
-    //     // Un hash trt5-XX peut être dans l'url. 
-    //     // Dans ce cas, on active l'item de navigation du carousel correspondant.
-    //     if (window.location.hash.substr(1).match("^trt5-")) {
-    //         $nav.eq(window.location.hash.substr(6) - 1).addClass("active");
-    //     } else {
-    //         // Sinon le premier item
-    //         $nav.eq(0).addClass("active");
-    //     }
-    //     // initialisation du carousel
-    //     $("#carouselPresentation").owlCarousel({
-    //         items:1,
-    //         center: true,
-    //         dots: false,
-    //         URLhashListener:true
-    //         //autoplay: true,
-    //         //autoplayTimeout: 6000,
-    //         //autoplayHoverPause: true,
-    //         //themeClass: 'owl-trt5'
-    //     }).on('changed.owl.carousel',function(e){
-    //         if (!e.namespace) return;
-    //         var actif = e.relatedTarget.current();
-    //         $nav.removeClass("active").eq(actif).addClass("active");
-    //     });
-    // }
 
     // Carousel actualités
     $("#carouselActus,#carouselPresentation").owlCarousel({
         items:1
     });
-
-
-
-    // $("#carouselActus").owlCarousel({
-    //     items:1,
-    //     center: true,
-    //     autoplay: true,
-    //     autoplayTimeout: 3000,
-    //     autoplayHoverPause: true,
-    //     //themeClass: 'owl-trt5'
-    // });
 
     // Foundation : offcanvas
     // ======================================
