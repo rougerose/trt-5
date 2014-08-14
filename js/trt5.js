0b2edd9a075364e9cbbe2cc1f548dde04684dd1e
christophe le drean <christophe@rouge-rose.net>
2014-08-14 17:56:35 +0200

    js : version 1 de owl qui permet une compatibilité avec jquery 1.7 installée avec spip. La version jquery 1.9 nécessaire pour faire fonctionner la version 2 de OWL empêche le formulaire de contact de fonctionner normalement.

--- js/trt5.js
+++ js/trt5.js
@@ -5,44 +5,69 @@ $(function() {
 
     // Carousel de présentation du TRT-5
     // =============================================
-
-    if ($("#carouselPresentation").length) {
-        var $nav = $("#carouselNav > a");
-        // Un hash trt5-XX peut être dans l'url. 
-        // Dans ce cas, on active l'item de navigation du carousel correspondant.
-        if (window.location.hash.substr(1).match("^trt5-")) {
-            $nav.eq(window.location.hash.substr(6) - 1).addClass("active");
-        } else {
-            // Sinon le premier item
-            $nav.eq(0).addClass("active");
-        }
-        // initialisation du carousel
-        $("#carouselPresentation").owlCarousel({
+    
+    if($("#carouselPresentation").length) {
+        var presentation = $("#carouselPresentation"), nav = $("#carouselNav > a");
+        nav.eq(0).addClass("active");
+        presentation.owlCarousel({
             items:1,
-            center: true,
-            dots: false,
-            URLhashListener:true
-            //autoplay: true,
-            //autoplayTimeout: 6000,
-            //autoplayHoverPause: true,
-            //themeClass: 'owl-trt5'
-        }).on('changed.owl.carousel',function(e){
-            if (!e.namespace) return;
-            var actif = e.relatedTarget.current();
-            $nav.removeClass("active").eq(actif).addClass("active");
+            pagination: false
+        });
+        nav.on("click",function(e){
+            e.preventDefault();
+            id = this.hash.substr(6) - 1;
+            presentation.trigger("owl.goTo",id);
+            nav.removeClass("active").eq(id).addClass("active");
         });
     }
+    
+    
+    
+
+    
+    
+    // if ($("#carouselPresentation").length) {
+    //     var $nav = $("#carouselNav > a");
+    //     // Un hash trt5-XX peut être dans l'url. 
+    //     // Dans ce cas, on active l'item de navigation du carousel correspondant.
+    //     if (window.location.hash.substr(1).match("^trt5-")) {
+    //         $nav.eq(window.location.hash.substr(6) - 1).addClass("active");
+    //     } else {
+    //         // Sinon le premier item
+    //         $nav.eq(0).addClass("active");
+    //     }
+    //     // initialisation du carousel
+    //     $("#carouselPresentation").owlCarousel({
+    //         items:1,
+    //         center: true,
+    //         dots: false,
+    //         URLhashListener:true
+    //         //autoplay: true,
+    //         //autoplayTimeout: 6000,
+    //         //autoplayHoverPause: true,
+    //         //themeClass: 'owl-trt5'
+    //     }).on('changed.owl.carousel',function(e){
+    //         if (!e.namespace) return;
+    //         var actif = e.relatedTarget.current();
+    //         $nav.removeClass("active").eq(actif).addClass("active");
+    //     });
+    // }
 
     // Carousel actualités
-
-    $("#carouselActus").owlCarousel({
-        items:1,
-        center: true,
-        autoplay: true,
-        autoplayTimeout: 3000,
-        autoplayHoverPause: true,
-        //themeClass: 'owl-trt5'
+    $("#carouselActus,#carouselPresentation").owlCarousel({
+        items:1
     });
+
+
+
+    // $("#carouselActus").owlCarousel({
+    //     items:1,
+    //     center: true,
+    //     autoplay: true,
+    //     autoplayTimeout: 3000,
+    //     autoplayHoverPause: true,
+    //     //themeClass: 'owl-trt5'
+    // });
 
     // Foundation : offcanvas
     // ======================================
